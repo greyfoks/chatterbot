048e9177df6834f4f5278fd696552bf8aac39175
cnetten <chris.netten@altrac.io>
2024-07-31 14:32:46 -0400

    Update page.js

--- app/(dashboard)/chat/page.js
+++ app/(dashboard)/chat/page.js
@@ -1,14 +1,18 @@
 import Chat from "@/components/Chat";
-import { dehydrate , HydrationBoundary, QueryClient} from "@tanstack/react-query";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
 const ChatPage = () => {
-    const queryClient = new QueryClient();
-    return (
-        <HydrationBoundary state={dehydrate(queryClient)}>
-        <div className="text-base-content">
-            <Chat />
-        </div>
-        </HydrationBoundary>
-    );
-}
+  const queryClient = new QueryClient();
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <div className="text-base-content">
+        <Chat />
+      </div>
+    </HydrationBoundary>
+  );
+};
 
 export default ChatPage;
